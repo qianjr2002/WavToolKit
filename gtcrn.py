09185b9a1777c961648be80426a3f772d707a37c
qianjr2002 <2132087637@qq.com>
2024-09-09 10:21:46 +0800

    update gtcrn fix a bug

--- gtcrn.py
+++ gtcrn.py
@@ -11,11 +11,14 @@ from einops import rearrange
 class ERB(nn.Module):
     def __init__(self, erb_subband_1, erb_subband_2, nfft=512, high_lim=8000, fs=16000):
         super().__init__()
-        erb_filters = self.erb_filter_banks(erb_subband_1, erb_subband_2, nfft, high_lim, fs)
+        erb_filters = self.erb_filter_banks(
+            erb_subband_1, erb_subband_2, nfft, high_lim, fs)
         nfreqs = nfft//2 + 1
         self.erb_subband_1 = erb_subband_1
-        self.erb_fc = nn.Linear(nfreqs-erb_subband_1, erb_subband_2, bias=False)
-        self.ierb_fc = nn.Linear(erb_subband_2, nfreqs-erb_subband_1, bias=False)
+        self.erb_fc = nn.Linear(nfreqs-erb_subband_1,
+                                erb_subband_2, bias=False)
+        self.ierb_fc = nn.Linear(
+            erb_subband_2, nfreqs-erb_subband_1, bias=False)
         self.erb_fc.weight = nn.Parameter(erb_filters, requires_grad=False)
         self.ierb_fc.weight = nn.Parameter(erb_filters.T, requires_grad=False)
 
@@ -33,27 +36,29 @@ class ERB(nn.Module):
         erb_high = self.hz2erb(high_lim)
         erb_points = np.linspace(erb_low, erb_high, erb_subband_2)
         bins = np.round(self.erb2hz(erb_points)/fs*nfft).astype(np.int32)
-        erb_filters = np.zeros([erb_subband_2, nfft // 2 + 1], dtype=np.float32)
+        erb_filters = np.zeros(
+            [erb_subband_2, nfft // 2 + 1], dtype=np.float32)
 
         erb_filters[0, bins[0]:bins[1]] = (bins[1] - np.arange(bins[0], bins[1]) + 1e-12) \
-                                                / (bins[1] - bins[0] + 1e-12)
+            / (bins[1] - bins[0] + 1e-12)
         for i in range(erb_subband_2-2):
             erb_filters[i + 1, bins[i]:bins[i+1]] = (np.arange(bins[i], bins[i+1]) - bins[i] + 1e-12)\
-                                                    / (bins[i+1] - bins[i] + 1e-12)
-            erb_filters[i + 1, bins[i+1]:bins[i+2]] = (bins[i+2] - np.arange(bins[i+1], bins[i + 2])  + 1e-12) \
-                                                    / (bins[i + 2] - bins[i+1] + 1e-12)
+                / (bins[i+1] - bins[i] + 1e-12)
+            erb_filters[i + 1, bins[i+1]:bins[i+2]] = (bins[i+2] - np.arange(bins[i+1], bins[i + 2]) + 1e-12) \
+                / (bins[i + 2] - bins[i+1] + 1e-12)
 
-        erb_filters[-1, bins[-2]:bins[-1]+1] = 1- erb_filters[-2, bins[-2]:bins[-1]+1]
-        
+        erb_filters[-1, bins[-2]:bins[-1]+1] = 1 - \
+            erb_filters[-2, bins[-2]:bins[-1]+1]
+
         erb_filters = erb_filters[:, erb_subband_1:]
         return torch.from_numpy(np.abs(erb_filters))
-    
+
     def bm(self, x):
         """x: (B,C,T,F)"""
         x_low = x[..., :self.erb_subband_1]
         x_high = self.erb_fc(x[..., self.erb_subband_1:])
         return torch.cat([x_low, x_high], dim=-1)
-    
+
     def bs(self, x_erb):
         """x: (B,C,T,F_erb)"""
         x_erb_low = x_erb[..., :self.erb_subband_1]
@@ -63,19 +68,23 @@ class ERB(nn.Module):
 
 class SFE(nn.Module):
     """Subband Feature Extraction"""
+
     def __init__(self, kernel_size=3, stride=1):
         super().__init__()
         self.kernel_size = kernel_size
-        self.unfold = nn.Unfold(kernel_size=(1,kernel_size), stride=(1, stride), padding=(0, (kernel_size-1)//2))
-        
+        self.unfold = nn.Unfold(kernel_size=(1, kernel_size), stride=(
+            1, stride), padding=(0, (kernel_size-1)//2))
+
     def forward(self, x):
         """x: (B,C,T,F)"""
-        xs = self.unfold(x).reshape(x.shape[0], x.shape[1]*self.kernel_size, x.shape[2], x.shape[3])
+        xs = self.unfold(x).reshape(
+            x.shape[0], x.shape[1]*self.kernel_size, x.shape[2], x.shape[3])
         return xs
 
 
 class TRA(nn.Module):
     """Temporal Recurrent Attention"""
+
     def __init__(self, channels):
         super().__init__()
         self.att_gru = nn.GRU(channels, channels*2, 1, batch_first=True)
@@ -85,8 +94,8 @@ class TRA(nn.Module):
     def forward(self, x):
         """x: (B,C,T,F)"""
         zt = torch.mean(x.pow(2), dim=-1)  # (B,C,T)
-        at = self.att_gru(zt.transpose(1,2))[0]
-        at = self.att_fc(at).transpose(1,2)
+        at = self.att_gru(zt.transpose(1, 2))[0]
+        at = self.att_fc(at).transpose(1, 2)
         at = self.att_act(at)
         At = at[..., None]  # (B,C,T,1)
 
@@ -97,36 +106,39 @@ class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1, use_deconv=False, is_last=False):
         super().__init__()
         conv_module = nn.ConvTranspose2d if use_deconv else nn.Conv2d
-        self.conv = conv_module(in_channels, out_channels, kernel_size, stride, padding, groups=groups)
+        self.conv = conv_module(in_channels, out_channels,
+                                kernel_size, stride, padding, groups=groups)
         self.bn = nn.BatchNorm2d(out_channels)
         self.act = nn.Tanh() if is_last else nn.PReLU()
+
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
 
 
 class GTConvBlock(nn.Module):
     """Group Temporal Convolution"""
+
     def __init__(self, in_channels, hidden_channels, kernel_size, stride, padding, dilation, use_deconv=False):
         super().__init__()
         self.use_deconv = use_deconv
         self.pad_size = (kernel_size[0]-1) * dilation[0]
         conv_module = nn.ConvTranspose2d if use_deconv else nn.Conv2d
-    
+
         self.sfe = SFE(kernel_size=3, stride=1)
-        
+
         self.point_conv1 = conv_module(in_channels//2*3, hidden_channels, 1)
         self.point_bn1 = nn.BatchNorm2d(hidden_channels)
         self.point_act = nn.PReLU()
 
         self.depth_conv = conv_module(hidden_channels, hidden_channels, kernel_size,
-                                            stride=stride, padding=padding,
-                                            dilation=dilation, groups=hidden_channels)
+                                      stride=stride, padding=padding,
+                                      dilation=dilation, groups=hidden_channels)
         self.depth_bn = nn.BatchNorm2d(hidden_channels)
         self.depth_act = nn.PReLU()
 
         self.point_conv2 = conv_module(hidden_channels, in_channels//2, 1)
         self.point_bn2 = nn.BatchNorm2d(in_channels//2)
-        
+
         self.tra = TRA(in_channels//2)
 
     def shuffle(self, x1, x2):
@@ -148,31 +160,36 @@ class GTConvBlock(nn.Module):
 
         h1 = self.tra(h1)
 
-        x =  self.shuffle(h1, x2)
-        
+        x = self.shuffle(h1, x2)
+
         return x
 
 
 class GRNN(nn.Module):
     """Grouped RNN"""
+
     def __init__(self, input_size, hidden_size, num_layers=1, batch_first=True, bidirectional=False):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bidirectional = bidirectional
-        self.rnn1 = nn.GRU(input_size//2, hidden_size//2, num_layers, batch_first=batch_first, bidirectional=bidirectional)
-        self.rnn2 = nn.GRU(input_size//2, hidden_size//2, num_layers, batch_first=batch_first, bidirectional=bidirectional)
+        self.rnn1 = nn.GRU(input_size//2, hidden_size//2, num_layers,
+                           batch_first=batch_first, bidirectional=bidirectional)
+        self.rnn2 = nn.GRU(input_size//2, hidden_size//2, num_layers,
+                           batch_first=batch_first, bidirectional=bidirectional)
 
     def forward(self, x, h=None):
         """
         x: (B, seq_length, input_size)
         h: (num_layers, B, hidden_size)
         """
-        if h== None:
+        if h == None:
             if self.bidirectional:
-                h = torch.zeros(self.num_layers*2, x.shape[0], self.hidden_size, device=x.device)
+                h = torch.zeros(self.num_layers*2,
+                                x.shape[0], self.hidden_size, device=x.device)
             else:
-                h = torch.zeros(self.num_layers, x.shape[0], self.hidden_size, device=x.device)
+                h = torch.zeros(self.num_layers,
+                                x.shape[0], self.hidden_size, device=x.device)
         x1, x2 = torch.chunk(x, chunks=2, dim=-1)
         h1, h2 = torch.chunk(h, chunks=2, dim=-1)
         h1, h2 = h1.contiguous(), h2.contiguous()
@@ -181,47 +198,53 @@ class GRNN(nn.Module):
         y = torch.cat([y1, y2], dim=-1)
         h = torch.cat([h1, h2], dim=-1)
         return y, h
-    
-    
+
+
 class DPGRNN(nn.Module):
     """Grouped Dual-path RNN"""
+
     def __init__(self, input_size, width, hidden_size, **kwargs):
         super(DPGRNN, self).__init__(**kwargs)
         self.input_size = input_size
         self.width = width
         self.hidden_size = hidden_size
 
-        self.intra_rnn = GRNN(input_size=input_size, hidden_size=hidden_size//2, bidirectional=True)
+        self.intra_rnn = GRNN(input_size=input_size,
+                              hidden_size=hidden_size//2, bidirectional=True)
         self.intra_fc = nn.Linear(hidden_size, hidden_size)
         self.intra_ln = nn.LayerNorm((width, hidden_size), eps=1e-8)
 
-        self.inter_rnn = GRNN(input_size=input_size, hidden_size=hidden_size, bidirectional=False)
+        self.inter_rnn = GRNN(input_size=input_size,
+                              hidden_size=hidden_size, bidirectional=False)
         self.inter_fc = nn.Linear(hidden_size, hidden_size)
         self.inter_ln = nn.LayerNorm(((width, hidden_size)), eps=1e-8)
-    
+
     def forward(self, x):
         """x: (B, C, T, F)"""
-        ## Intra RNN
+        # Intra RNN
         x = x.permute(0, 2, 3, 1)  # (B,T,F,C)
-        intra_x = x.reshape(x.shape[0] * x.shape[1], x.shape[2], x.shape[3])  # (B*T,F,C)
+        intra_x = x.reshape(x.shape[0] * x.shape[1],
+                            x.shape[2], x.shape[3])  # (B*T,F,C)
         intra_x = self.intra_rnn(intra_x)[0]  # (B*T,F,C)
         intra_x = self.intra_fc(intra_x)      # (B*T,F,C)
-        intra_x = intra_x.reshape(x.shape[0], -1, self.width, self.hidden_size) # (B,T,F,C)
+        intra_x = intra_x.reshape(
+            x.shape[0], -1, self.width, self.hidden_size)  # (B,T,F,C)
         intra_x = self.intra_ln(intra_x)
         intra_out = torch.add(x, intra_x)
 
-        ## Inter RNN
-        x = intra_out.permute(0,2,1,3)  # (B,F,T,C)
-        inter_x = x.reshape(x.shape[0] * x.shape[1], x.shape[2], x.shape[3]) 
+        # Inter RNN
+        x = intra_out.permute(0, 2, 1, 3)  # (B,F,T,C)
+        inter_x = x.reshape(x.shape[0] * x.shape[1], x.shape[2], x.shape[3])
         inter_x = self.inter_rnn(inter_x)[0]  # (B*F,T,C)
         inter_x = self.inter_fc(inter_x)      # (B*F,T,C)
-        inter_x = inter_x.reshape(x.shape[0], self.width, -1, self.hidden_size) # (B,F,T,C)
-        inter_x = inter_x.permute(0,2,1,3)   # (B,T,F,C)
-        inter_x = self.inter_ln(inter_x) 
+        inter_x = inter_x.reshape(
+            x.shape[0], self.width, -1, self.hidden_size)  # (B,F,T,C)
+        inter_x = inter_x.permute(0, 2, 1, 3)   # (B,T,F,C)
+        inter_x = self.inter_ln(inter_x)
         inter_out = torch.add(intra_out, inter_x)
-        
-        dual_out = inter_out.permute(0,3,1,2)  # (B,C,T,F)
-        
+
+        dual_out = inter_out.permute(0, 3, 1, 2)  # (B,C,T,F)
+
         return dual_out
 
 
@@ -229,11 +252,16 @@ class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.en_convs = nn.ModuleList([
-            ConvBlock(3*3, 16, (1,5), stride=(1,2), padding=(0,2), use_deconv=False, is_last=False),
-            ConvBlock(16, 16, (1,5), stride=(1,2), padding=(0,2), groups=2, use_deconv=False, is_last=False),
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(0,1), dilation=(1,1), use_deconv=False),
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(0,1), dilation=(2,1), use_deconv=False),
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(0,1), dilation=(5,1), use_deconv=False)
+            ConvBlock(3*3, 16, (1, 5), stride=(1, 2), padding=(0, 2),
+                      use_deconv=False, is_last=False),
+            ConvBlock(16, 16, (1, 5), stride=(1, 2), padding=(0, 2),
+                      groups=2, use_deconv=False, is_last=False),
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                0, 1), dilation=(1, 1), use_deconv=False),
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                0, 1), dilation=(2, 1), use_deconv=False),
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                0, 1), dilation=(5, 1), use_deconv=False)
         ])
 
     def forward(self, x):
@@ -248,11 +276,16 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.de_convs = nn.ModuleList([
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(2*5,1), dilation=(5,1), use_deconv=True),
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(2*2,1), dilation=(2,1), use_deconv=True),
-            GTConvBlock(16, 16, (3,3), stride=(1,1), padding=(2*1,1), dilation=(1,1), use_deconv=True),
-            ConvBlock(16, 16, (1,5), stride=(1,2), padding=(0,2), groups=2, use_deconv=True, is_last=False),
-            ConvBlock(16, 2, (1,5), stride=(1,2), padding=(0,2), use_deconv=True, is_last=True)
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                2*5, 1), dilation=(5, 1), use_deconv=True),
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                2*2, 1), dilation=(2, 1), use_deconv=True),
+            GTConvBlock(16, 16, (3, 3), stride=(1, 1), padding=(
+                2*1, 1), dilation=(1, 1), use_deconv=True),
+            ConvBlock(16, 16, (1, 5), stride=(1, 2), padding=(
+                0, 2), groups=2, use_deconv=True, is_last=False),
+            ConvBlock(16, 2, (1, 5), stride=(1, 2), padding=(
+                0, 2), use_deconv=True, is_last=True)
         ])
 
     def forward(self, x, en_outs):
@@ -260,16 +293,17 @@ class Decoder(nn.Module):
         for i in range(N_layers):
             x = self.de_convs[i](x + en_outs[N_layers-1-i])
         return x
-    
+
 
 class Mask(nn.Module):
     """Complex Ratio Mask"""
+
     def __init__(self):
         super().__init__()
 
     def forward(self, mask, spec):
-        s_real = spec[:,0] * mask[:,0] - spec[:,1] * mask[:,1]
-        s_imag = spec[:,1] * mask[:,0] + spec[:,0] * mask[:,1]
+        s_real = spec[:, 0] * mask[:, 0] - spec[:, 1] * mask[:, 1]
+        s_imag = spec[:, 1] * mask[:, 0] + spec[:, 0] * mask[:, 1]
         s = torch.stack([s_real, s_imag], dim=1)  # (B,2,T,F)
         return s
 
@@ -281,10 +315,10 @@ class GTCRN(nn.Module):
         self.sfe = SFE(3, 1)
 
         self.encoder = Encoder()
-        
+
         self.dpgrnn1 = DPGRNN(16, 33, 16)
         self.dpgrnn2 = DPGRNN(16, 33, 16)
-        
+
         self.decoder = Decoder()
 
         self.mask = Mask()
@@ -295,26 +329,27 @@ class GTCRN(nn.Module):
         """
         spec_ref = spec  # (B,F,T,2)
 
-        spec_real = spec[..., 0].permute(0,2,1)
-        spec_imag = spec[..., 1].permute(0,2,1)
+        spec_real = spec[..., 0].permute(0, 2, 1)
+        spec_imag = spec[..., 1].permute(0, 2, 1)
         spec_mag = torch.sqrt(spec_real**2 + spec_imag**2 + 1e-12)
-        feat = torch.stack([spec_mag, spec_real, spec_imag], dim=1)  # (B,3,T,257)
+        feat = torch.stack([spec_mag, spec_real, spec_imag],
+                           dim=1)  # (B,3,T,257)
 
         feat = self.erb.bm(feat)  # (B,3,T,129)
         feat = self.sfe(feat)     # (B,9,T,129)
 
         feat, en_outs = self.encoder(feat)
-        
-        feat = self.dpgrnn1(feat) # (B,16,T,33)
-        feat = self.dpgrnn2(feat) # (B,16,T,33)
+
+        feat = self.dpgrnn1(feat)  # (B,16,T,33)
+        feat = self.dpgrnn2(feat)  # (B,16,T,33)
 
         m_feat = self.decoder(feat, en_outs)
-        
+
         m = self.erb.bs(m_feat)
 
-        spec_enh = self.mask(m, spec_ref.permute(0,3,2,1)) # (B,2,T,F)
-        spec_enh = spec_enh.permute(0,3,2,1)  # (B,F,T,2)
-        
+        spec_enh = self.mask(m, spec_ref.permute(0, 3, 2, 1))  # (B,2,T,F)
+        spec_enh = spec_enh.permute(0, 3, 2, 1)  # (B,F,T,2)
+
         return spec_enh
 
 
@@ -324,7 +359,7 @@ if __name__ == "__main__":
     """complexity count"""
     from ptflops import get_model_complexity_info
     flops, params = get_model_complexity_info(model, (257, 63, 2), as_strings=True,
-                                           print_per_layer_stat=True, verbose=True)
+                                              print_per_layer_stat=True, verbose=True)
     print(flops, params)
 
     """causality check"""
@@ -333,13 +368,19 @@ if __name__ == "__main__":
     c = torch.randn(1, 16000)
     x1 = torch.cat([a, b], dim=1)
     x2 = torch.cat([a, c], dim=1)
-    
-    x1 = torch.stft(x1, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
-    x2 = torch.stft(x2, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
+
+    x1 = torch.stft(x1, 512, 256, 512, torch.hann_window(
+        512).pow(0.5), return_complex=False)
+    x2 = torch.stft(x2, 512, 256, 512, torch.hann_window(
+        512).pow(0.5), return_complex=False)
     y1 = model(x1)[0]
     y2 = model(x2)[0]
-    y1 = torch.istft(y1, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
-    y2 = torch.istft(y2, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
-    
+    z1 = torch.complex(y1[..., 0], y1[..., 1])
+    z2 = torch.complex(y2[..., 0], y2[..., 1])
+    y1 = torch.istft(z1, 512, 256, 512, torch.hann_window(512).pow(0.5))
+    y2 = torch.istft(z2, 512, 256, 512, torch.hann_window(512).pow(0.5))
+    # y1 = torch.istft(y1, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
+    # y2 = torch.istft(y2, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False)
+
     print((y1[:16000-256*2] - y2[:16000-256*2]).abs().max())
     print((y1[16000:] - y2[16000:]).abs().max())
