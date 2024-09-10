07ae37dfc13f654fcda1c4ab824be9c3e8bcac66
qianjr2002 <2132087637@qq.com>
2024-09-10 14:35:58 +0800

    add eSTOI

--- calculate_STOI.py
+++ calculate_STOI.py
@@ -6,10 +6,13 @@ import scipy.io.wavfile as wav
 from pystoi.stoi import stoi
 
 '''
+STOI:
 python calculate_STOI.py -c wav/clean/ -e wav/gtcrn_enh/
+eSTOI:
+python calculate_STOI.py -c wav/clean/ -e wav/gtcrn_enh/ --extended
 '''
 
-def calculate_stoi(clean_file, enhanced_file):
+def calculate_stoi(clean_file, enhanced_file, extended):
     fs_clean, clean_signal = wav.read(clean_file)
     fs_enhanced, enhanced_signal = wav.read(enhanced_file)
     
@@ -20,12 +23,13 @@ def calculate_stoi(clean_file, enhanced_file):
     clean_signal = clean_signal[:min_len]
     enhanced_signal = enhanced_signal[:min_len]
     
-    stoi_value = stoi(clean_signal, enhanced_signal, fs_clean, extended=False)
+    stoi_value = stoi(clean_signal, enhanced_signal, fs_clean, extended=extended)
     return stoi_value
 
-def calculate_average_stoi(clean_folder, enhanced_folder):
+def calculate_average_stoi(clean_folder, enhanced_folder, extended):
     stoi_list = []
-    for clean_file in tqdm(os.listdir(clean_folder), desc='Average STOI calculating'):
+    desc = 'Average eSTOI calculating' if extended else 'Average STOI calculating'
+    for clean_file in tqdm(os.listdir(clean_folder), desc=desc):
         if clean_file.endswith('.wav'):
             clean_path = os.path.join(clean_folder, clean_file)
             enhanced_path = os.path.join(enhanced_folder, clean_file)
@@ -34,24 +38,28 @@ def calculate_average_stoi(clean_folder, enhanced_folder):
                 print(f"Enhanced file not found for: {clean_path}")
                 continue
 
-            stoi_value = calculate_stoi(clean_path, enhanced_path)
+            stoi_value = calculate_stoi(clean_path, enhanced_path, extended)
             stoi_list.append(stoi_value)
             
             # print(f"File: {clean_file}, STOI: {stoi_value}")
 
     if stoi_list:
         average_stoi = sum(stoi_list) / len(stoi_list)
-        print("Average STOI:", average_stoi)
+        if extended:
+            print("Average eSTOI:", average_stoi)
+        else:
+            print("Average STOI:", average_stoi)
     else:
         print("No STOI values calculated. Check if the folders contain matching WAV files.")
 
 def main():
-    parser = argparse.ArgumentParser(description="Calculate average STOI for WAV files in specified folders.")
+    parser = argparse.ArgumentParser(description="Calculate average STOI or eSTOI for WAV files in specified folders.")
     parser.add_argument('--clean_folder', '-c', type=str, required=True, help="Path to the folder containing clean WAV files")
     parser.add_argument('--enhanced_folder', '-e', type=str, required=True, help="Path to the folder containing enhanced WAV files")
+    parser.add_argument('--extended', action='store_true', help="Calculate eSTOI if this flag is set (default: STOI)")
     args = parser.parse_args()
     
-    calculate_average_stoi(args.clean_folder, args.enhanced_folder)
+    calculate_average_stoi(args.clean_folder, args.enhanced_folder, args.extended)
 
 if __name__ == "__main__":
     main()
--- calculate_wavfile_STOI.py
+++ calculate_wavfile_STOI.py
@@ -8,10 +8,13 @@ https://github.com/mpariente/pystoi
 '''
 
 '''
+STOI:
 python calculate_wavfile_STOI.py -c wav/clean/p232_005.wav -e wav/gtcrn_enh/p232_005.wav
+eSTOI:
+python calculate_wavfile_STOI.py -c wav/clean/p232_005.wav -e wav/gtcrn_enh/p232_005.wav --extended
 '''
 
-def calculate_stoi(clean_file, enhanced_file):
+def calculate_stoi(clean_file, enhanced_file, extended):
     fs_clean, clean_signal = wav.read(clean_file)
     fs_enhanced, enhanced_signal = wav.read(enhanced_file)
     
@@ -22,17 +25,23 @@ def calculate_stoi(clean_file, enhanced_file):
     clean_signal = clean_signal[:min_len]
     enhanced_signal = enhanced_signal[:min_len]
     
-    stoi_value = stoi(clean_signal, enhanced_signal, fs_clean, extended=False)
+    stoi_value = stoi(clean_signal, enhanced_signal, fs_clean, extended=extended)
     return stoi_value
 
 def main():
-    parser = argparse.ArgumentParser(description='Calculate STOI between clean and enhanced signal')
+    parser = argparse.ArgumentParser(description='Calculate STOI or eSTOI between clean and enhanced signal')
     parser.add_argument('--clean', '-c', required=True, help='Path to the clean signal WAV file')
     parser.add_argument('--enhanced', '-e', required=True, help='Path to the enhanced signal WAV file')
+    parser.add_argument('--extended', action='store_true', help='Calculate eSTOI if this flag is set (default: False for STOI)')
     args = parser.parse_args()
 
-    stoi_value = calculate_stoi(args.clean, args.enhanced)
-    print(f'STOI: {stoi_value}')
+    # extended will be True if the flag is set, otherwise False
+    stoi_value = calculate_stoi(args.clean, args.enhanced, args.extended)
+    
+    if args.extended:
+        print(f'eSTOI: {stoi_value}')
+    else:
+        print(f'STOI: {stoi_value}')
 
 if __name__ == '__main__':
     main()
